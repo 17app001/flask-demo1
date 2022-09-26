4c2b0819a1e410412a7b0dfac949aa4208596278
jerrychen <jerry@gmail.com>
2022-09-26 21:05:13 +0800

    完成六都繪製&繪製函示化

--- static/js/pm25-charts.js
+++ static/js/pm25-charts.js
@@ -1,4 +1,6 @@
 let chart = echarts.init(document.querySelector("#main"));
+let chart1 = echarts.init(document.querySelector("#six"));
+
 let pm25HighSite = document.querySelector("#pm25_high_site");
 let pm25HighValue = document.querySelector("#pm25_high_value");
 let pm25LowSite = document.querySelector("#pm25_low_site");
@@ -9,8 +11,29 @@ let dateEl = document.querySelector("#date");
 
 $(document).ready(() => {
     drawPM25();
+    drawSixPM25();
 });
 
+
+
+function drawSixPM25() {
+    chart1.showLoading();
+    $.ajax(
+        {
+            url: "/pm25-six-json",
+            type: "POST",
+            dataType: "json",
+            success: (data) => {
+                chart1.hideLoading();
+                drawChart(data["site"], data["pm25"], "PM2.5六都平均值", chart1);
+            },
+            error: () => {
+                chart1.hideLoading();
+                alert("讀取資料失敗!");
+            }
+        }
+    );
+}
 
 // 呼叫ajax(jQuery)
 function drawPM25() {
@@ -18,13 +41,11 @@ function drawPM25() {
     $.ajax(
         {
             url: "/pm25-json",
-            type: "GET",
+            type: "POST",
             dataType: "json",
             success: (data) => {
                 chart.hideLoading();
-                console.log(data);
-                // drawPM25Chart(data);
-                drawPM25Chart(data);
+                drawChart(data["site"], data["pm25"], "PM2.5全省示意圖", chart);
                 renderMaxPM25(data);
             },
             error: () => {
@@ -55,102 +76,12 @@ function renderMaxPM25(data) {
     dateEl.innerText = data["date"];
 }
 
-function drawPM25Chart2(data2) {
-    var chartDom = document.getElementById('main');
-    var myChart = echarts.init(chartDom);
-    var option;
 
-    // prettier-ignore
-    let dataAxis = data2["site"];
-
-    // prettier-ignore
-    let data = data2["pm25"];
-    let yMax = 500;
-    let dataShadow = [];
-    for (let i = 0; i < data.length; i++) {
-        dataShadow.push(yMax);
-    }
-    option = {
-        title: {
-            text: '特性示例：渐变色 阴影 点击缩放',
-            subtext: 'Feature Sample: Gradient Color, Shadow, Click Zoom'
-        },
-        xAxis: {
-            data: dataAxis,
-            axisLabel: {
-                inside: true,
-                color: '#fff'
-            },
-            axisTick: {
-                show: false
-            },
-            axisLine: {
-                show: false
-            },
-            z: 10
-        },
-        yAxis: {
-            axisLine: {
-                show: false
-            },
-            axisTick: {
-                show: false
-            },
-            axisLabel: {
-                color: '#999'
-            }
-        },
-        dataZoom: [
-            {
-                type: 'inside'
-            }
-        ],
-        series: [
-            {
-                type: 'bar',
-                showBackground: true,
-                itemStyle: {
-                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                        { offset: 0, color: '#83bff6' },
-                        { offset: 0.5, color: '#188df0' },
-                        { offset: 1, color: '#188df0' }
-                    ])
-                },
-                emphasis: {
-                    itemStyle: {
-                        color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                            { offset: 0, color: '#2378f7' },
-                            { offset: 0.7, color: '#2378f7' },
-                            { offset: 1, color: '#83bff6' }
-                        ])
-                    }
-                },
-                data: data
-            }
-        ]
-    };
-    // Enable data zoom when user click bar.
-    const zoomSize = 6;
-    myChart.on('click', function (params) {
-        console.log(dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)]);
-        myChart.dispatchAction({
-            type: 'dataZoom',
-            startValue: dataAxis[Math.max(params.dataIndex - zoomSize / 2, 0)],
-            endValue:
-                dataAxis[Math.min(params.dataIndex + zoomSize / 2, data.length - 1)]
-        });
-    });
-
-    option && myChart.setOption(option);
-
-}
-
-
-function drawPM25Chart(data) {
+function drawChart(xdata, ydata, title = "", chart = null) {
     // 指定图表的配置项和数据
     let option = {
         title: {
-            text: 'PM2.5 全圖數據圖'
+            text: title
         },
         toolbox: {
             show: true,
@@ -170,14 +101,14 @@ function drawPM25Chart(data) {
             data: ['PM2.5']
         },
         xAxis: {
-            data: data["site"]
+            data: xdata
         },
         yAxis: {},
         series: [
             {
                 name: 'PM2.5',
                 type: 'bar',
-                data: data["pm25"]
+                data: ydata
             }
         ]
     };
